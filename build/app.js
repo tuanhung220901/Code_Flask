455cc72ef8621c277f6a5ec072afcad4d3c77fd6
Tuan_Hung <hoangtuanhung220901@gmail.com>
2021-06-24 14:33:00 +0700

    first

--- build/app.js
+++ build/app.js
@@ -1,6 +1,4 @@
-import CodeFlask from 'codeflask';
-
-const flask = new CodeFlask('.test', {
+const flask = new CodeFlask('#code-1', {
     language: 'C++',
     lineNumbers: true,
     // areaId: 'thing1',
@@ -26,3 +24,17 @@ print(L[2:7])
 // print(L[-7:-2])
 // # Prints ['c', 'd', 'e', 'f', 'g']
 // `);
+const flask1 = new CodeFlask('#code-2', {
+  language: 'C++',
+  lineNumbers: true,
+  // areaId: 'thing1',
+  // ariaLabelledby: 'header1',
+  //handleTabs: true,
+  readonly: true,
+});
+
+flask1.updateCode(`L = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(L[2:7])
+# Prints ['c', 'd', 'e', 'f', 'g']
+hihi
+`);
